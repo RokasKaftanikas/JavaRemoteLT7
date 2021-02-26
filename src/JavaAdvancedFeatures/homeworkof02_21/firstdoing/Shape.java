e4140bb867b1745223b2fb11b6ab2d59b30d59f8
RokasKaftanikas <rokas.kaft@gmail.com>
2021-02-26 12:39:34 +0200

    add first 02-21 homework exercise

---
+++ src/JavaAdvancedFeatures/homeworkof02_21/firstdoing/Shape.java
@@ -0,0 +1,23 @@
+package JavaAdvancedFeatures.homeworkof02_21.firstdoing;
+
+public abstract class Shape {
+    /*
+    Susikurti abstrakčią klasę `Shape` ir jį paveldėti:
+    * `Shape` klasė privalo turėti lauką: `shapeDescription`;
+    * Susikurti objektą `Circle` su lauku `radius`;
+    * Susikurti objektą `RightAngleRectangle` su laukais `width`, `height`;
+    * Susikurti objektą `Rectangle` ir paveldėti objektą RightAngleRectangle;
+    * Inicializuoti sukurtus `Circle`, `RightAngleRectangle` ir `Rectangle` objektus;
+    * Apskaičiuoti apskritimo plotą ir ilgį;
+    * Apskaičiuoti stataus trikampio plotą - ```(a*b)/2``` ir stačiakampio plotą;
+    * Gautus rezultatus įrašyti į failą naudojantis BufferedWriter objekta.
+     */
+
+    protected String shapeDescription;
+
+    public Shape(String shapeDescription){
+        this.shapeDescription = shapeDescription;
+    }
+    public abstract String getArea();
+
+}
